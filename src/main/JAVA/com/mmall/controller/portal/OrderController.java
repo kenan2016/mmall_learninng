9edc2dddc1e7a20b517e6d4981e5c4b8d1a19eaf
kenan2016 <524871763@qq.com>
2018-10-31 22:42:31 +0800

    支付å宝回调

--- src/main/JAVA/com/mmall/controller/portal/OrderController.java
+++ src/main/JAVA/com/mmall/controller/portal/OrderController.java
@@ -1,18 +1,21 @@
 package com.mmall.controller.portal;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author kenan
@@ -23,7 +26,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/order")
 public class OrderController {
-
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
     @Autowired
     private IOrderService iOrderService;
 
@@ -49,4 +52,21 @@ public class OrderController {
     }
 
     // 支付宝的回调函数
+    public Object alipayCallback(HttpServletRequest request) {
+        Map<String, String> params = Maps.newHashMap();
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values =   (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i<values.length; i++) {
+                valueStr = (i == values.length - 1)? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+
+        logger.info("支付宝回调，sign:{},trade_status:{},参数：{}",params.get("sign"),params.get("trade_status"), params.toString());
+        // 非常重要。验证回调的正确性，是不是支付宝发的，并且还要回避重复通知
+
+    }
 }
